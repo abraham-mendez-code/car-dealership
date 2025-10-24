7fc7d667d2362e47a5f970b21ae193e88dd6503a
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-24 14:12:32 -0400

    Added the Program class

---
+++ src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        // create a new UserInterface instance
+        UserInterface ui = new UserInterface();
+
+        // call the display method
+        ui.display();
+
+    }
+
+}
