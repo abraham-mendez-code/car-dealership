b6e8f2e889f0e52a695f65277c3df0c0769d577b
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-24 11:58:56 -0400

    Added the DealershipFileManager class. Fixed typo in variable name for the Vehicle class. Added the inventory.csv.

---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DealershipFileManager {
+
+    // private attributes
+    private static final String inventoryDir = "src/main/resources/inventory.csv";
+
+    // private constructor
+    private DealershipFileManager() {}
+
+    // this method reads and loads the inventory file into a list of Vehicles and returns a Dealership
+    public static Dealership getDealership() {
+
+
+        return null;
+
+    }
+
+
+    public static void saveDealership(Dealership dealership) {
+
+
+
+    }
+
+}
