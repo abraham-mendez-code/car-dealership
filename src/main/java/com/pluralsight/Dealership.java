608f798671d1a83677f3dfb1c6c9cb001b29fbc2
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-24 11:33:29 -0400

    Added the Dealership class

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,75 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+
+    // private attributes
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> vehicles;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.vehicles = new ArrayList<>();
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {
+
+        return null;
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {
+
+        return null;
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max) {
+
+        return null;
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+
+        return null;
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMilage(double min, double max) {
+
+        return null;
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {
+
+        return null;
+
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+
+        return null;
+
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+
+
+
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+
+
+    }
+
+}
