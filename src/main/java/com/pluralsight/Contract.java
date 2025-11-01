b3222cc35af0680f4a97db4e4b2d21126952fc33
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-31 20:12:29 -0400

    Added the abstract Contract class

---
+++ src/main/java/com/pluralsight/Contract.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+public abstract class Contract {
+
+    // class attributes
+    String date;
+    String customerName;
+    String customerEmail;
+    String vehicleSold;
+    double totalPrice;
+    double monthlyPayment;
+
+    // constructor
+    public Contract (String date, String customerName, String customerEmail, String vehicleSold ) {
+
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+
+    }
+
+    // getter methods
+    public String getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    // setter methods
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setVehicleSold(String vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+}
