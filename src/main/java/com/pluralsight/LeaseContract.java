aae9f30fd71c77503acb8ea05c07fe579bdce89d
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-31 21:15:28 -0400

    Updated LeaseContract.getMonthlyPayment, this now returns the monthly payment.

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -41,6 +41,26 @@ public class LeaseContract extends Contract{
 
     @Override
     public double getMonthlyPayment() {
-        return 0;
+
+        /*
+            Standard loan payment formula:
+                M = (r x P) / 1 - (1 + r)^-n
+            M = monthly payment
+            P = principle (price or loan amount)
+            r = monthly interest rate (annual rate / 12)
+            n = total number of payments (months)
+
+            All leases are financed at 4.0% for 36 months
+         */
+
+        // declare formula variables
+        double m;
+        double p = vehicleSold.getPrice(); // assign the vehicle price
+        double r = .04 / 12;
+        double n = 36;
+
+        m = (r * p) / (1 - Math.pow((1 + r), -n)); // calculate monthly payment
+
+        return m;
     }
 }
